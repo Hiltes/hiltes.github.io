6acf02e0b38a06435738a4f8e6f37270b1820f0f
Hiltes <szymanekdariusz448@gmail.com>
2024-12-01 16:23:07 +0100

    komentarze

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,9 @@
 const starsContainer = document.querySelector('.stars');
-for (let i = 0; i < 200; i++) { // Liczba gwiazd
+for (let i = 0; i < 200; i++) { 
     const star = document.createElement('div');
     star.classList.add('star');
-    star.style.top = Math.random() * 100 + '%'; // Losowa pozycja
+    star.style.top = Math.random() * 100 + '%'; 
     star.style.left = Math.random() * 100 + '%';
-    star.style.animationDelay = Math.random() * 2 + 's'; // Losowy czas opóźnienia
+    star.style.animationDelay = Math.random() * 2 + 's'; 
     starsContainer.appendChild(star);
 }
